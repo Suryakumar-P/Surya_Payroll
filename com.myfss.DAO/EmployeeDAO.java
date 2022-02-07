0d8360fd986a5cca286de769454ad50721ff6341
Suryakumar-P <98871009+Suryakumar-P@users.noreply.github.com>
2022-02-07 15:32:36 +0530

    Add files via upload

--- com.myfss.DAO/EmployeeDAO.java
+++ com.myfss.DAO/EmployeeDAO.java
@@ -4,23 +4,45 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.myfss.Beans.Employee;
-import com.mysql.cj.Query;
+import com.myfss.Beans.Login;
 
 public class EmployeeDAO {
 
 	public static int deleteEmp(String EID) throws SQLException {
-		String query= "DELETE FROM login_details where id='"+EID+"'";
+		String query=String.format("DELETE FROM login_details where id='%s'",EID);
 		return MySqlCon.execueDML(query);
 	}
 
 	public static int insertEmp(Employee emp) throws SQLException {
-		String query="insert into ";
+		String query=String.format
+				("insert into user_personal_details(firstname,lastname,phone,email,dob,login_id,position,doj,employee_grade,team,company_name) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')", 
+						emp.getFirstName(),emp.getLastName(),emp.getPhoneNumber(),emp.getEmail(),emp.getDob(),
+						emp.getEmployeeId(),emp.getPosition(),emp.getDoj(),emp.getEmployeeGrade(),emp.getTeam(),emp.getCompanyName());	
 		return MySqlCon.execueDML(query);
 	}
 	
-	public static String password(String EID,String password) {
-		String query = "select password,is_admin from login_details where id'"+EID+"'";
-		return query;
+	public static Login passwordVerification(String EID) throws SQLException {
+		String query =String.format("select * from login_details where id='%s'",EID);
+		ResultSet result=MySqlCon.select(query);
+		while(result.next()) {
+			return new Login(result.getString("username"),result.getString("password"),result.getString("account_type"),result.getString("id"));
+		}
+		return new Login();
+	}
+	
+	public static Employee viewEmployee(String EID) throws SQLException {
+		String query=String.format("select *from employee_personal_details where login_id='%s'",EID);
+		ResultSet result=MySqlCon.select(query);
+		while(result.next()) {
+		return new Employee(result.getString("login_id"),result.getString("firstname"),result.getString("lastname"),result.getString("email_address"),result.getString("phone"),result.getString("dob"),result.getString("position"),result.getString("doj"),result.getString("employee_grade"),result.getString("team"),result.getString("comapny_name"));
+		}
+		return new Employee();
+	}
+	
+	public static int updateEmp(Employee emp) throws SQLException {
+		String query=String.format("update user_personal_details set firstname='%s',lastname='%s',phone='%s',email='%s',dob='%s',position='%s',doj='%s',employee_grade='%s',team='%s',company_name='%s' where login_id='%s'", emp.getFirstName(),emp.getLastName(),emp.getPhoneNumber(),emp.getEmail(),emp.getDob(),
+						emp.getPosition(),emp.getDoj(),emp.getEmployeeGrade(),emp.getTeam(),emp.getCompanyName(),emp.getEmployeeId());
+		return MySqlCon.execueDML(query);
 	}
 	
 	
