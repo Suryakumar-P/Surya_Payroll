f1785a2b81a5cc176dade8060ae33bd2eab259fe
Suryakumar-P <98871009+Suryakumar-P@users.noreply.github.com>
2022-02-08 14:54:00 +0530

    Add files via upload

--- com.myfss.DAO/EmployeeDAO.java
+++ com.myfss.DAO/EmployeeDAO.java
@@ -3,6 +3,8 @@ package com.myfss.DAO;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.text.DefaultEditorKit.CopyAction;
+
 import com.myfss.Beans.Employee;
 import com.myfss.Beans.EmployeePayDetails;
 import com.myfss.Beans.EmployeePayStandard;
@@ -11,11 +13,13 @@ import com.myfss.Beans.Payslip;
 
 public class EmployeeDAO {
 
+	//Deletes Employee with the given EID
 	public static int deleteEmp(String EID) throws SQLException {
 		String query=String.format("DELETE FROM login_details where id='%s'",EID);
 		return MySqlCon.execueDML(query);
 	}
 
+	//Insert employee to the table
 	public static int insertEmp(Employee emp) throws SQLException {
 		String query=String.format
 				("insert into user_personal_details(firstname,lastname,phone,email,dob,login_id,position,doj,employee_grade,team,company_name) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')", 
@@ -24,6 +28,7 @@ public class EmployeeDAO {
 		return MySqlCon.execueDML(query);
 	}
 	
+	//Returns the login details of the EID given
 	public static Login passwordVerification(String EID) throws SQLException {
 		String query =String.format("select * from login_details where id='%s'",EID);
 		ResultSet result=MySqlCon.select(query);
@@ -33,6 +38,7 @@ public class EmployeeDAO {
 		return new Login();
 	}
 	
+	//Returns employee details of the employee given
 	public static Employee viewEmployee(String EID) throws SQLException {
 		String query=String.format("select *from employee_personal_details where login_id='%s'",EID);
 		ResultSet result=MySqlCon.select(query);
@@ -42,13 +48,14 @@ public class EmployeeDAO {
 		return new Employee();
 	}
 	
+	//Updates employee details with the given details and Employee id
 	public static int updateEmp(Employee emp) throws SQLException {
 		String query=String.format("update user_personal_details set firstname='%s',lastname='%s',phone='%s',email='%s',dob='%s',position='%s',doj='%s',employee_grade='%s',team='%s',company_name='%s' where login_id='%s'", emp.getFirstName(),emp.getLastName(),emp.getPhoneNumber(),emp.getEmail(),emp.getDob(),
 						emp.getPosition(),emp.getDoj(),emp.getEmployeeGrade(),emp.getTeam(),emp.getCompanyName(),emp.getEmployeeId());
 		return MySqlCon.execueDML(query);
 	}
 	
-	
+	//Returns generated payslip details
 	public static Payslip viewPaySlip(String EID) throws SQLException {
 		String query=String.format("select *from payslip from where login_id='%s'", EID);
 		ResultSet result=MySqlCon.select(query);
@@ -58,8 +65,9 @@ public class EmployeeDAO {
 		return new Payslip();
 	}
 	
+	//Returns standards of the pay for the employee grade
 	public static EmployeePayStandard viewEmployeePay(String EID) throws SQLException {
-		String query=String.format("select * from payslip where login_id='%s'", EID);
+		String query=String.format("select * from employee_pay where employee_grade in (select employee_grade from user_personal_details where login_id='%s')", EID);
 		ResultSet result=MySqlCon.select(query);
 		while(result.next()) {
 			return new EmployeePayStandard(result.getString("employee_grade"),result.getFloat("basic_pay"),result.getFloat("tax_percentage"),result.getFloat("pf_percentage"),result.getFloat("per_hour_pay"),result.getFloat("per_hour_overtime_pay"),result.getFloat("total_allowances"));
@@ -67,15 +75,17 @@ public class EmployeeDAO {
 		return new EmployeePayStandard();
 	}
 	
-	public static EmployeePayDetails viewEmployeePayApplied(String EID) throws SQLException {
-		String query= String.format("select * from pay_details where user_id in (select user_id from user_personal_details where login_id='%s'", EID);
+	//Returns Employee pay like attendance hours allowances in the bean
+	public static EmployeePayDetails viewEmployeePayApplied(String EID,String date) throws SQLException {
+		String query= String.format("select * from pay_details where user_id in (select user_id from user_personal_details where login_id='%s') and month_of_application='%s'", EID,date);
 		ResultSet result=MySqlCon.select(query);
 		while(result.next()) {
-			return new EmployeePayDetails(result.getFloat(0),result.getFloat(0),result.getString(0),result.getFloat(0));
+			return new EmployeePayDetails(result.getFloat("hours_worked"),result.getFloat("overtime_hours"),result.getString("month_of_application"),result.getFloat("allowance_applied"));
 		}
 		return new EmployeePayDetails();
 	}
 	
+	//Insert into employee pay details like hours
 	public static int insertEmployeePayDetails(EmployeePayDetails emp,String EID) throws SQLException {
 		String selectQuery=String.format("select user_id from user_personal_details where login_id='%s'", EID);
 		ResultSet resultSet=MySqlCon.select(selectQuery);
@@ -86,4 +96,12 @@ public class EmployeeDAO {
 		String query=String.format("insert into pay_details (hours_worked,overtime_hours,month_of_application,allowance_applied,user_id) values(%f,%f,'%s',%f,%d)", emp.getHoursWorked(),emp.getOvertimeHours(),emp.getMonthOfApplication(),emp.getAllowanceApplied(),userId);
 		return MySqlCon.execueDML(query);
 	}
+	
+	//Insert generated payslip into table
+	public static int insertPayslip(Payslip pay,String eId) throws SQLException {
+		String query= String.format("insert into payslip (total_pay,basic_pay,pf_deducted,tax_deducted,overtime_pay,allowances,hour_pay,month_of_pay,login_id) values(%f,%f,%f,%f,%f,%f,%f,'%s','%s')", pay.getTotalPay(),pay.getBasicPay(),pay.getPfDeducted(),pay.getTaxDeducted(),pay.getOvertimePay(),pay.getAllowances(),pay.getHourPay(),pay.getMonthOfPay(),eId);
+		return MySqlCon.execueDML(query);
+	}
+		
 }
+
--- com.myfss.DAO/MySqlCon.java
+++ com.myfss.DAO/MySqlCon.java
@@ -9,21 +9,25 @@ import java.sql.Statement;
 
 public class MySqlCon {
 
+		//create a static connection
 	public static Connection conn;
-	public static void main(String[] args) {
+	static {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_system","root","password");
 		} catch (Exception e) {
-			e.printStackTrace();
-		}  
+				e.printStackTrace();
+		}
 	}
-	
+		
+		
+	//JDBC code for select statment execution
 	public static ResultSet select(String query) throws SQLException {
 		Statement statement = conn.createStatement();
 		return statement.executeQuery(query);
 	}
 	
+	//JDBC code for DML statements execution
 	public static int execueDML(String query) throws SQLException {
 		PreparedStatement statement = conn.prepareStatement(query);
 		return statement.executeUpdate();
