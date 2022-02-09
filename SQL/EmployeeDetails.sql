22fb0e660314d278031787219035d3037a01be99
Saiguptha98 <52167149+Saiguptha98@users.noreply.github.com>
2022-02-09 11:03:35 +0530

    Add files via upload

---
+++ SQL/EmployeeDetails.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from project.employeeview
+LIMIT 0, 1000
+
+-- Date: 2022-02-09 11:02
+*/
+INSERT INTO `` (`empid`,`empname`,`empemail`,`empsal`,`empphone`,`position`,`employeeGrade`,`companyname`,`teams`) VALUES (10,'John','john@gmail.com',55000.00,8957300000,'Software Engg','M1','TCS','Project team');
+INSERT INTO `` (`empid`,`empname`,`empemail`,`empsal`,`empphone`,`position`,`employeeGrade`,`companyname`,`teams`) VALUES (11,'Gopal','gopal@gmail.com',3000.00,9398560000,'Java Developer','M2','Wipro','phases of teamwork');
+INSERT INTO `` (`empid`,`empname`,`empemail`,`empsal`,`empphone`,`position`,`employeeGrade`,`companyname`,`teams`) VALUES (12,'prem','prem@gmail.com',15000.00,9807390000,'Business analyst','M3','SalePush','Self-managed teams');
+INSERT INTO `` (`empid`,`empname`,`empemail`,`empsal`,`empphone`,`position`,`employeeGrade`,`companyname`,`teams`) VALUES (13,'akhil','akhil@gmail.com',25000.00,9398570000,'Executive Assistant.','M4','Google India','Cross-functional teams');
+INSERT INTO `` (`empid`,`empname`,`empemail`,`empsal`,`empphone`,`position`,`employeeGrade`,`companyname`,`teams`) VALUES (14,'Ajay','ajay@gmail.com',50000.00,89674400000,'Web Designer','M5','IBM India','Troubleshooting teams');
