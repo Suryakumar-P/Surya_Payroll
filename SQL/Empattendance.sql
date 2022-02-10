fbe6450eb24952c766ef7b8339ec25c54038dc03
Saiguptha98 <52167149+Saiguptha98@users.noreply.github.com>
2022-02-10 15:34:58 +0530

    Add files via upload

---
+++ SQL/Empattendance.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: select * from project.empatendance
+LIMIT 0, 1000
+
+-- Date: 2022-02-10 15:29
+*/
+create table empatendance(Empid int primary key,EmpWorkinghours int ,EmpDateWorked int,NoOfLeavestaken int);
+select * from project.empatendance;
+
+INSERT INTO `` (`Empid`,`EmpWorkinghours`,`EmpDateWorked`,`NoOfLeavestaken`) VALUES (16021,8,30,5);
