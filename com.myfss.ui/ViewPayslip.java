e0a48fe4f5c0442a386b62e99bf2e2badce8cc33
Shivaa-Ramakrishnan <98874532+Shivaa-Ramakrishnan@users.noreply.github.com>
2022-02-10 15:46:54 +0530

    Add files via upload

---
+++ com.myfss.ui/ViewPayslip.java
@@ -0,0 +1,47 @@
+package com.myfss.ui;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.myfss.beans.*;
+
+public class ViewPayslip {
+
+	/**
+	 * @param args
+	 */public static boolean viewPayslip() throws IOException
+	 {
+		 try
+		 {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String employeeId;
+		System.out.println("Enter the Employee ID to view the payslip:");
+		employeeId = br.readLine();
+		System.out.println("Enter the month of the payslip");
+		String dateString=br.readLine();
+		 Employee e = EmployeeDAO.viewEmployee(employeeId);
+			if((e.getEmployeeId).equals(employeeId))
+			{
+			Payslip p = EmployeeDAO.viewPaySlip(employeeId,dateString);
+			System.out.println(p);
+			br.close();
+			return true;
+			}
+			else
+			{
+				System.out.println("The employee Id is invalid. Please Enter a valid Employee Id.");
+				return false;
+			}	 
+		 }
+		 catch(Exception e)
+		 {
+			 System.out.println(e);
+		 }
+	 }
+	
+	public static void main(String[] args) {
+		
+	}
+}
