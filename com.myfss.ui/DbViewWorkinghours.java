28a7481a4629f51a074eaff386c7c903395e53e4
Saiguptha98 <52167149+Saiguptha98@users.noreply.github.com>
2022-02-10 23:42:50 +0530

    Add files via upload

---
+++ com.myfss.ui/DbViewWorkinghours.java
@@ -0,0 +1,52 @@
+package FSS.ui;
+
+
+
+	
+	import java.io.BufferedReader;
+	import java.io.InputStreamReader;
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.PreparedStatement;
+	import java.sql.ResultSet;
+
+	public class DbViewWorkinghours {
+	private static int empid;
+
+	public static void main (String[] args) throws Exception {
+			
+			
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");   
+	        float hours_worked = 0,overtime_hours,month_of_application,allowance_applied;
+	       // int emp_id = 0;
+			System.out.println("Enter Hours_worked :");
+			//emp_id=Integer.parseInt(br.readLine());
+			hours_worked=Float.parseFloat(br.readLine());
+			
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","abcd");
+			
+			PreparedStatement stmt=con.prepareStatement("select * from empatendance");
+
+		    
+		    
+		    
+			ResultSet rs = stmt.executeQuery();
+			if(rs!= null) {
+				System.out.println("   Hours_worked     Overtime_hours    Month_of_application   Allowance_applied");
+				System.out.println(  "           8             2                        2020-04-5               1 ");
+				
+			
+			while(rs.next())
+				
+			System.out.println(rs.getFloat(2)+" "+rs.getFloat(3)+"                 "+rs.getString(4)+"        "+rs.getFloat(5));
+			
+			
+			}
+			}catch(Exception e) {System.out.println(e);
+			}
+				}
+	}
+
